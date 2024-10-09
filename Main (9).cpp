6b08d0e8624b7783edf29f565ae30b600cade530
ADP Assignment <nagarekrishna16@gmail.com>
2024-10-09 05:31:53 +0530

    Add files via upload

--- Main (9).cpp
+++ Main (9).cpp
@@ -1,13 +1,20 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-	int first_number, second_number, sum;
-	cout << "Enter two integers:";
-	cin >> first_number >> second_number;
-	
-	sum = first_number + second_number;
-	
-	cout << first_number << "+" << second_number << "=" << sum;
-	return 0;
+#include <iostream> 
+using namespace std; 
+class Base { 
+public: 
+virtual void print() { 
+cout << "Base Function" << endl; 
+} 
+}; 
+class Derived : public Base { 
+public: 
+void print() { 
+cout << "Derived Function" << endl; 
+} 
+}; 
+int main() { 
+Derived derived1; 
+// Call print() function of Derived class 
+derived1.print(); 
+return 0; 
 }
